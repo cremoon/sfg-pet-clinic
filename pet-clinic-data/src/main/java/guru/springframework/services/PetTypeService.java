40970c135c938be4d5c1f357ba5beafd965a1e9e
cremoon <bartels.maik@gmail.com>
2020-07-22 09:20:33 +0200

    Added PetTypeService classes. Closes #29

---
+++ pet-clinic-data/src/main/java/guru/springframework/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package guru.springframework.services;
+
+import guru.springframework.model.PetType;
+
+/**
+ * Created by maikbartels on 2020.07.22
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
