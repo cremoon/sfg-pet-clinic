b461808e631dcd102cffb67cacda5a72f16fabb4
cremoon <bartels.maik@gmail.com>
2020-07-22 08:59:27 +0200

    Added visits, updated owner with pet relationship. Closes #26

--- pet-clinic-data/src/main/java/guru/springframework/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/model/Owner.java
@@ -1,7 +1,19 @@
 package guru.springframework.model;
 
+import java.util.Set;
+
 /**
  * Created by maikbartels on 2020.07.18
  */
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
---
+++ pet-clinic-data/src/main/java/guru/springframework/model/Visit.java
@@ -0,0 +1,37 @@
+package guru.springframework.model;
+
+import java.time.LocalDate;
+
+/**
+ * Created by maikbartels on 2020.07.22
+ */
+public class Visit extends BaseEntity {
+
+    private LocalDate date;
+    private String description;
+    private Pet pet;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+}
