c7b27b2fef21802c2133cc909ae801df6ab0465d
cremoon <bartels.maik@gmail.com>
2020-07-22 15:08:29 +0200

    Added SpecialitiesMapService impl. Closes #31

---
+++ pet-clinic-data/src/main/java/guru/springframework/services/SpecialitiesService.java
@@ -0,0 +1,9 @@
+package guru.springframework.services;
+
+import guru.springframework.model.Speciality;
+
+/**
+ * Created by maikbartels on 2020.07.22
+ */
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
