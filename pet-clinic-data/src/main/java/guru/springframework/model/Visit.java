d175c029d0179196fa576508b8ee80049147c391
cremoon <bartels.maik@gmail.com>
2020-07-24 09:30:23 +0200

    Updated visit with JPA entity mappings. Closes #38

--- pet-clinic-data/src/main/java/guru/springframework/model/Visit.java
+++ pet-clinic-data/src/main/java/guru/springframework/model/Visit.java
@@ -1,14 +1,23 @@
 package guru.springframework.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * Created by maikbartels on 2020.07.22
  */
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
