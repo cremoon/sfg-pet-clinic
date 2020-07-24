b2c12556e08e874174a284b499d6e918b2e788e9
cremoon <bartels.maik@gmail.com>
2020-07-24 09:20:30 +0200

    Updated vet with JPA entity mappings. Closes #37

--- pet-clinic-data/src/main/java/guru/springframework/model/Speciality.java
+++ pet-clinic-data/src/main/java/guru/springframework/model/Speciality.java
@@ -1,10 +1,17 @@
 package guru.springframework.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by maikbartels on 2020.07.22
  */
+@Entity
+@Table(name = "speciality")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
