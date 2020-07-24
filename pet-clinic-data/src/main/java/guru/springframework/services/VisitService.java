3d7c824160fb8de4af612990a9507a244fc93f35
cremoon <bartels.maik@gmail.com>
2020-07-24 13:44:53 +0200

    Implement map service for Visit. Closes #46

---
+++ pet-clinic-data/src/main/java/guru/springframework/services/VisitService.java
@@ -0,0 +1,9 @@
+package guru.springframework.services;
+
+import guru.springframework.model.Visit;
+
+/**
+ * Created by maikbartels on 2020.07.20
+ */
+public interface VisitService extends CrudService<Visit, Long>{
+}
