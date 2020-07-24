99712117841813edfccd374c785dd87ff7c3758e
cremoon <bartels.maik@gmail.com>
2020-07-24 10:08:35 +0200

    Added spring data jpa service for owner. Closes #40

--- pet-clinic-data/src/main/java/guru/springframework/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/repositories/OwnerRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.repository.CrudRepository;
  * Created by maikbartels on 2020.07.24
  */
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String name);
 }
